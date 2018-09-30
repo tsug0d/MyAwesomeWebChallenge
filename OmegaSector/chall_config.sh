56623a8b49b0a6ad4c398a485ae934f60ae23e95
Nguyen Nguyen <tsublogs@gmail.com>
2018-10-01 03:30:42 +0700

    Update chall_config.sh

--- OmegaSector/chall_config.sh
+++ OmegaSector/chall_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "configuring OmegaSector..."
-docker-compose up -d
-wait $(pgrep docker-compose)
-docker exec -it $(docker ps -aqf "name=omegasector_php") chmod 777 /var/www/html/alien_message
-docker exec -it $(docker ps -aqf "name=omegasector_php") chmod 777 /var/www/html/human_message
+echo "configuring OmegaSector...";
+docker-compose up -d;
+wait $(pgrep docker-compose);
+docker exec -it $(docker ps -aqf "name=omegasector_php") chmod 777 /var/www/html/alien_message;
+docker exec -it $(docker ps -aqf "name=omegasector_php") chmod 777 /var/www/html/human_message;
